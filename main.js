fc198154ed4d44cebcb7d5fe024a0cb9e42cf112
Daniel Maslov <maslov4real@gmail.com>
2023-02-05 10:12:06 +0200

    rendering pic and img

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 'use strict'
 
-function onAsk() {
-    getAns(renderAns)
+$(onInit)
+
+function onInit() {
+    $('.user-question').on('input', debounce(onGetAns, 500))
 }
 
-function renderAns(ans) {
-    console.log('ans:', ans);
-    
-// document.querySelector()
-
+function onGetAns(ev) {
+    console.log('getting')
+    const quest = $('.user-question').val()
+    if (quest.length >= 3 && quest.endsWith('?')) {
+        getAns(renderAns)
+    }
 }
 
+function renderAns({ answer, image }) {
+    $('.answer').html(answer)
+    $('.res-img').attr('src', image)
+    checkAnswer(answer)
+}
+
+function checkAnswer(ans) {
+    if (ans === 'yes') getJoke(renderJoke)
+    else getDogImg(renderDogImg)
+}
+
+function renderJoke({ value: joke }) {
+    $(".joke").html(joke) 
+}
+
+function renderDogImg({ message: dogImg }) {
+    $('.dog-img').attr('src', dogImg)
+}
